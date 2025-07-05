223e537670fd87733b56bed26692d071353a5cb3
RamonSVieira <ramonvieira122333@gmail.com>
2025-07-04 23:39:57 -0300

    fix: api-url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/'; 
+const API_URL = 'http://ec2-3-90-66-5.compute-1.amazonaws.com:3000/'; 
 
 const apiClient = axios.create({
   baseURL: API_URL,
