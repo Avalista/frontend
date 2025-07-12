b1e006c55550e314b4e820214ae2f7189360b218
radmilags <gama.radmila@gmail.com>
2025-07-12 16:25:35 -0300

    fix: base url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://ec2-3-90-66-5.compute-1.amazonaws.com:3000/'; 
+const API_URL = 'http://ec2-98-84-191-1.compute-1.amazonaws.com'; 
 
 const apiClient = axios.create({
   baseURL: API_URL,
